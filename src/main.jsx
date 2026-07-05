c7b955057f667d5de3d34f7c8c94cfcacb502a39
liliosee1 <liliosee.1@gmaile.com>
2026-07-05 21:14:03 +0200

    setup main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import { CartProvider } from './context/CartContext'
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
 const queryClient = new QueryClient({
@@ -18,11 +19,13 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </CartProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 )
